5b398b55dc0eef78f3288c15bcdc501df179aa92
Ethan Colbert <emcolbert2@gmail.com>
2019-10-18 16:39:36 -0400

    The two BC programs will compute combinations correctly.

--- src/edu/bellarmine/emc/BCDynamic.java
+++ src/edu/bellarmine/emc/BCDynamic.java
@@ -1,5 +1,7 @@
 package edu.bellarmine.emc;
 
+import java.util.Scanner;
+
 /**
  * This class implements the dynamic version of binomial coefficient calculation.
  * 
@@ -10,13 +12,45 @@ package edu.bellarmine.emc;
  */
 
 public class BCDynamic {
-
+	
+	static long n, k, result;
+	
 	/**
 	 * Execution starts here.
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("This is the dynamic version of BC.");//making sure I'm testing the right program.
+		Scanner input = new Scanner(System.in);
 		
+		do {
+			System.out.print("Enter the number of choices: ");
+			n = input.nextLong();
+			System.out.print("Enter how many to choose: ");
+			k = input.nextLong();
+		} while (!(k > 0 && n >= k));
+		
+		long numerator = 1, denominator1 = 1, denominator2 = 1;
+		
+		for (int i = 1; i <= n; i++) {
+			
+			numerator *= i;
+			
+			if (i == k) {
+				denominator1 = numerator;
+			}
+			
+			if (i == (n - k)) {
+				denominator2 = numerator;
+			}
+			
+		}
+		
+		result = (numerator / (denominator1 * denominator2));
+		
+		System.out.printf("There are %d ways to choose %d items from %d choices.", result, k, n);
+		
+		input.close();
 	}// end main
 
 }// end "BCDynamic" class
--- src/edu/bellarmine/emc/BCRecursive.java
+++ src/edu/bellarmine/emc/BCRecursive.java
@@ -1,6 +1,7 @@
 package edu.bellarmine.emc;
 
 import java.math.BigInteger;
+import java.util.Scanner;
 
 /**
  * This class implements the recursive version of binomial coefficient calculation.
@@ -12,13 +13,53 @@ import java.math.BigInteger;
  */
 
 public class BCRecursive {
-
+	
+	static long n, k, result;
+	
 	/**
 	 * Execution starts here.
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("This is the recursive version of BC.");//making sure I'm testing the right program.
+		Scanner input = new Scanner(System.in);
 		
+		do {
+			System.out.print("Enter the number of choices: ");
+			n = input.nextLong();
+			System.out.print("Enter how many to choose: ");
+			k = input.nextLong();
+		} while (!(k > 0 && n >= k));
+		
+		long numerator = factorial(n);
+		long denominator1 = factorial(k);
+		long denominator2 = factorial(n - k);
+		
+		result = (numerator / (denominator1 * denominator2));
+		
+		System.out.printf("There are %d ways to choose %d items from %d choices.", result, k, n);
+		
+		input.close();
 	}// end main
-
+	
+	/**
+	 * SUPPORT METHOD - returns the factorial of the number passed in.
+	 * Will return -1 if the number passed in is negative.
+	 * @param input - a positive integer
+	 * @return the factorial of input
+	 */
+	private static long factorial(long input) {
+		
+		if (input < 0) {
+			return -1;
+		}
+		else if (input == 0 || input == 1) {
+			return 1;
+		}
+		else {
+			return (input * factorial(input - 1));
+		}
+		
+	}// end "factorial" support method
+	
 }// end "BCRecursive" class
